81ae3b1a9bb5f0ad9fe7f52c4ef6a7b462368825
sofiahartveit <58219884+sofiahartveit@users.noreply.github.com>
2021-02-16 15:38:28 +0100

    AdminKundeController ferdig ida sofia

--- src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
+++ src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
@@ -6,7 +6,9 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.internal.matchers.Any;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.web.bind.annotation.RequestBody;
 import oslomet.testing.API.AdminKundeController;
 import oslomet.testing.DAL.AdminRepository;
 import oslomet.testing.Models.Konto;
@@ -55,8 +57,7 @@ public class EnhetstestAdminKundeController {
 
         assertEquals(kunde, resultat);
     }
-
-    // Ida og Sofia: Vi tror denne er riktig
+    
     @Test
     public void hentAlle_IkkeLoggetInn() {
 
@@ -67,62 +68,62 @@ public class EnhetstestAdminKundeController {
         assertNull(resultat);
     }
 
-    //
-
     @Test
     public void lagreKunde_loggetInn() {
 
-        // arrange
+        Kunde enKunde = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
+
+        Mockito.when(sjekk.loggetInn()).thenReturn("01010110523");
+
+        Mockito.when(repository.registrerKunde(enKunde)).thenReturn("OK");
 
         // act
+        String resultat = adminKundeController.lagreKunde(enKunde);
 
         // assert
-
+        assertEquals("OK", resultat);
     }
 
     @Test
     public void lagreKunde_ikkeLoggetInn() {
-        // arrange
 
-        // act
+        when(sjekk.loggetInn()).thenReturn(null);
 
-        // assert
+        String resultat = adminKundeController.lagreKunde(null);
 
-    }
+        assertEquals("Ikke logget inn", resultat);
+        }
 
     @Test
     public void endre_loggetInn() {
-        // arrange
+        Kunde enKunde = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
 
+        Mockito.when(sjekk.loggetInn()).thenReturn("01010110523");
+
+        Mockito.when(repository.endreKundeInfo(enKunde)).thenReturn("OK");
 
         // act
+        String resultat = adminKundeController.endre(enKunde);
 
         // assert
-
+        assertEquals("OK", resultat);
     }
-
 
     @Test
     public void endre_ikkeLoggetInn() {
-        // arrange
 
-        // act
+        when(sjekk.loggetInn()).thenReturn(null);
 
-        // assert
+        String resultat = adminKundeController.endre(null);
+
+        assertEquals("Ikke logget inn", resultat);
     }
 
-    /*
-     @GetMapping("/slett")
-    public String slett(String personnummer) {
-        String p = sjekk.loggetInn();
-        if (p !=null) {
-            return repository.slettKunde(personnummer);
-        }
-        return "Ikke logget inn";
-    }
-     */
 
-    // Vi prøvde denne, men denne er også feil:(
     @Test
     public void slett_loggetInn() {
 
@@ -141,7 +142,6 @@ public class EnhetstestAdminKundeController {
         // assert
         assertEquals("OK", resultat);
     }
-
 
     @Test
     public void slett_ikkeLoggetInn() {
